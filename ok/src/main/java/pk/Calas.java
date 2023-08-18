757b43d1cdfc191d3f98f4953f259f168cbf66ed
osrikanthyadav <rockzzsri82@gmail.com>
2023-08-18 14:36:40 +0530

    first commit

--- ok/src/main/java/pk/Calas.java
+++ ok/src/main/java/pk/Calas.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,15 +14,18 @@ public class Calas extends HttpServlet
 {
 	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
 	{
-		int i=Integer.parseInt(req.getParameter("n1"));
-		int j=Integer.parseInt(req.getParameter("n2"));
-		int k=i+j;
+		res.setContentType("text/html");
+		String k=req.getParameter("n1");
+		
 		PrintWriter out=res.getWriter();
 		out.println("da");
 		out.println("result is "+k);
-		req.setAttribute("k", k);
-		
-		RequestDispatcher rd=req.getRequestDispatcher("uk");
-		rd.forward(req, res);
+	//	req.setAttribute("k", k);
+		Cookie c=new Cookie("k",k);
+		res.addCookie(c);
+		out.print("<a href='s2'>S2</a>");
+		out.print("<a href='servlet.pdf'>JAVA-BOOK</a>");
+//		RequestDispatcher rd=req.getRequestDispatcher("zzzz");
+//		rd.include(req, res);
 	}
 }
--- ok/src/main/java/pk/SqServlet.java
+++ ok/src/main/java/pk/SqServlet.java
@@ -3,17 +3,25 @@ package pk;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+@WebServlet("/s2")
 public class SqServlet extends HttpServlet
 {
-	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
 	{
-		int k=(int)req.getAttribute("k");
+//		int i=Integer.parseInt(req.getParameter("n1"));
+//		int j=Integer.parseInt(req.getParameter("n2"));
+//		int k=i+j;
+		
+	//	int k=(int)req.getAttribute("k");
+		Cookie[] c=req.getCookies();
+		int k=Integer.parseInt(c[0].getValue());
 		PrintWriter out=res.getWriter();
-		out.println("Addition of two numbers= "+k);
+		out.println("Addition of two numbers= "+(k+k));
 		k=k*k;
 		out.println("Square for it = "+k);
 	}
